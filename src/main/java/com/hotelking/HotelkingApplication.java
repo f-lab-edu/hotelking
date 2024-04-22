f7f254b2af9e1c0f9f0ba8fdc519f855d8a657f8
kmss6905 <kmss69052@naver.com>
2024-04-22 22:21:50 +0900

    init

---
+++ src/main/java/com/hotelking/HotelkingApplication.java
@@ -0,0 +1,13 @@
+package com.hotelking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelkingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotelkingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/hotelking/HotelkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotelking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelkingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
