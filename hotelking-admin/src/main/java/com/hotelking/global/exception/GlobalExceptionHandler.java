ac63b1958a28b1563d549b394607dcde6e87553e
kmss6905 <kmss69052@naver.com>
2024-05-10 19:22:29 +0900

    Feat: 전역예외핸들러 수정

--- hotelking-admin/src/main/java/com/hotelking/global/exception/GlobalExceptionHandler.java
+++ hotelking-admin/src/main/java/com/hotelking/global/exception/GlobalExceptionHandler.java
@@ -1,50 +1,52 @@
 package com.hotelking.global.exception;
 
-import com.hotelking.global.ApiResponse;
-import com.hotelking.global.ErrorContent;
+import com.hotelking.exception.ErrorCode;
+import com.hotelking.exception.ErrorContent;
+import com.hotelking.exception.HotelkingException;
+import com.hotelking.response.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.util.ContentCachingRequestWrapper;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-  // 모두 validation 과정에서 생길 수 있는 예외
-  @ExceptionHandler({BindException.class, IllegalArgumentException.class})
-  public ResponseEntity<ApiResponse<ErrorContent>> handleArgumentNotValidException(Exception e, HttpServletRequest request) {
-    if (e instanceof BindException) {
-      BindingResult bindingResult = ((BindException) e).getBindingResult();
-      List<ErrorContent> errorContents = bindingResult.getAllErrors().stream()
-          .map(it ->
-              {
-                FieldError fieldError = (FieldError) it;
-                return ErrorContent.of(ErrorCode.findCode(fieldError.getDefaultMessage()),
-                    fieldError.getField());
-              }
-          ).toList();
-      log.warn("[validation error], exceptionClass = {}, uri = {}, errorContents = {}", e.getClass(), request.getRequestURI(), errorContents);
-      return ResponseEntity.status(400).body(ApiResponse.error(errorContents));
-    }
-
-    ErrorContent errorContent = ErrorContent.from(ErrorCode.findCode(e.getMessage()));
-    log.warn("[validation error], exceptionClass = {}, uri = {}, errorContent = {}", e.getClass(), request.getRequestURI(), errorContent);
-    return ResponseEntity.status(400)
-        .body(ApiResponse.error(errorContent));
+  @ExceptionHandler(HotelkingException.class)
+  public ResponseEntity<ApiResponse<ErrorContent>> handleHotelkingException(
+      HotelkingException e,
+      HttpServletRequest request
+  ){
+    ErrorCode ec = e.getErrorCode();
+    printLog(request, ec);
+    return ResponseEntity.status(ec.getHttpStatus()).body(ApiResponse.error(ErrorContent.from(ec)));
   }
 
   @ExceptionHandler(HttpMessageNotReadableException.class)
   public ResponseEntity<ApiResponse<ErrorContent>> handleHttpMessageNotReadableException(HttpMessageNotReadableException e, HttpServletRequest request) {
-    ErrorContent errorContent = ErrorContent.from(ErrorCode.NOT_READABLE);
-    log.warn("[ error], exceptionClass = {}, uri = {}, errorContent = {}", e.getClass(), request.getRequestURI(), errorContent);
-    return ResponseEntity.status(400).body(ApiResponse.error(errorContent));
+    ErrorCode ec = ErrorCode.NOT_READABLE;
+    printLog(request, ec);
+    return ResponseEntity
+        .status(ErrorCode.NOT_READABLE.getHttpStatus())
+        .body(ApiResponse.error(ErrorContent.from(ec)));
+  }
+
+  private void printLog(HttpServletRequest request, ErrorCode ec) {
+    if (request instanceof ContentCachingRequestWrapper wrapper) {
+      String body = new String(wrapper.getContentAsByteArray());
+      String params = wrapper.getQueryString();
+      log.error("code={} | internal_msg={} | status={} | method={} | uri={} | params={} | body={}",
+          ec.getCode(), ec.getInternalErrorMsg(), ec.getHttpStatus().value(), request.getMethod(),
+          request.getRequestURI(), params, body);
+    } else {
+      log.error("code={} | internal_msg={} | status={} | method={} | uri={}",
+          ec.getCode(), ec.getInternalErrorMsg(), ec.getHttpStatus().value(), request.getMethod(),
+          request.getRequestURI());
+    }
   }
 
 }
