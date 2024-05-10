51ea03e1bb82181891df744904ae04724ee517e6
kmss6905 <kmss69052@naver.com>
2024-05-11 00:59:12 +0900

    Chore: ApiResponse 주석 제거

--- hotelking-common/src/main/java/com/hotelking/response/ApiResponse.java
+++ hotelking-common/src/main/java/com/hotelking/response/ApiResponse.java
@@ -7,17 +7,14 @@ import com.hotelking.exception.ErrorContent;
 @JsonInclude(value = Include.NON_NULL)
 public record ApiResponse<T>(String result, T data, ErrorContent error) {
 
-  // {"result" : "success", data : { "hello": "world" }}
   public static <T> ApiResponse<T> success(T data) {
     return new ApiResponse<>("success", data, null);
   }
 
-  // {"result" : "success"}
   public static <T> ApiResponse<T> success() {
     return new ApiResponse<>("success", null, null);
   }
 
-  // {"result" : "error", "error" : { "code": "A001", "message": "invalid format"}}
   public static <T> ApiResponse<T> error(ErrorContent error) {
     return new ApiResponse<>("error", null, error);
   }
