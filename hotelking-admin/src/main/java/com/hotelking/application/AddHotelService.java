152b9ed93ca5d1a8f0e63687ec7cd644d4f95a44
kmss6905 <kmss69052@naver.com>
2024-05-16 21:21:25 +0900

    Build: hotelking-admin 공통 클래스 hotelking-common 밑으로 이동

---
+++ hotelking-admin/src/main/java/com/hotelking/application/AddHotelService.java
@@ -0,0 +1,19 @@
+package com.hotelking.application;
+
+import com.hotelking.infra.HotelRepository;
+import com.hotelking.dto.AddHotelDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddHotelService {
+
+  private final HotelRepository hotelRepository;
+
+  public AddHotelService(HotelRepository hotelRepository) {
+    this.hotelRepository = hotelRepository;
+  }
+
+  public void registerHotel(final AddHotelDto addHotel) {
+    hotelRepository.save(addHotel.toEntity());
+  }
+}
---
+++ hotelking-admin/src/main/java/com/hotelking/controller/HotelManagementController.java
@@ -0,0 +1,27 @@
+package com.hotelking.controller;
+
+import com.hotelking.dto.request.AddHotelRequest;
+import com.hotelking.response.ApiResponse;
+import com.hotelking.application.AddHotelService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class HotelManagementController {
+
+  private final AddHotelService addHotelService;
+
+  public HotelManagementController(AddHotelService addHotelService) {
+    this.addHotelService = addHotelService;
+  }
+
+  @PostMapping("/admin/hotels")
+  public ApiResponse<?> registerHotel(@RequestBody AddHotelRequest addHotelRequest) {
+    addHotelRequest.validationCheck();
+    addHotelService.registerHotel(addHotelRequest.toAddHotel());
+    return ApiResponse.success();
+  }
+}
