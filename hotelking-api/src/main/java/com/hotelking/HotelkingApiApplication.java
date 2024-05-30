c0a09312c2a4a999442f27e324b0e1e53dc3dce5
kmss6905 <kmss69052@naver.com>
2024-05-31 03:54:16 +0900

    Feat: [hotelking-api] 휴대폰 인증 요청 컨트롤러 추가

--- hotelking-api/src/main/java/com/hotelking/HotelkingApiApplication.java
+++ hotelking-api/src/main/java/com/hotelking/HotelkingApiApplication.java
@@ -1,11 +1,12 @@
 package com.hotelking;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HotelkingApiApplication {
 
   public static void main(String[] args) {
-
+    SpringApplication.run(HotelkingApiApplication.class, args);
   }
 }
---
+++ hotelking-api/src/main/java/com/hotelking/controller/UserController.java
@@ -0,0 +1,26 @@
+package com.hotelking.controller;
+
+import com.hotelking.application.AuthService;
+import com.hotelking.auth.PhoneAuthToken;
+import com.hotelking.dto.request.PhoneAuthCodeRequest;
+import com.hotelking.response.ApiResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+  private final AuthService authService;
+
+  public UserController(AuthService authService) {
+    this.authService = authService;
+  }
+
+  @PostMapping(value = "/phone-verification")
+  public ApiResponse<PhoneAuthToken> verifyPhone(@RequestBody PhoneAuthCodeRequest phoneAuthCodeRequest) {
+    phoneAuthCodeRequest.validationCheck();
+    PhoneAuthToken phoneAuthToken = authService.issuePhoneAuth(phoneAuthCodeRequest.toDto());
+    return ApiResponse.success(phoneAuthToken);
+  }
+}
