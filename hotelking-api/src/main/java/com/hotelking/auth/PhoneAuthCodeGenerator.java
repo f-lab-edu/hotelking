d07911ba52327303e3e43d12bbebf5f1002b9e5c
kmss6905 <kmss69052@naver.com>
2024-05-30 18:09:42 +0900

    Feat: [hotelking-api] Phone 인증코드 생성기 클래스 추가

---
+++ hotelking-api/src/main/java/com/hotelking/auth/PhoneAuthCodeGenerator.java
@@ -0,0 +1,14 @@
+package com.hotelking.auth;
+
+import java.util.Random;
+
+public class PhoneAuthCodeGenerator {
+
+  private static final Random random = new Random();
+
+  public static String generateAuthCode() {
+    int randomNumber = 100000 + random.nextInt(900000);
+    return String.valueOf(randomNumber);
+  }
+
+}
