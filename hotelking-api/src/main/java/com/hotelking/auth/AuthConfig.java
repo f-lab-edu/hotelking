179519e82f229be27c3880498632da1f8d7f34d2
김민식 <kmss69052@naver.com>
2024-05-31 21:55:45 +0900

    Feat: [hotelking-api] PasswordEncoder 빈 등록

--- hotelking-api/src/main/java/com/hotelking/auth/AuthConfig.java
+++ hotelking-api/src/main/java/com/hotelking/auth/AuthConfig.java
@@ -3,8 +3,10 @@ package com.hotelking.auth;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.encrypt.AesBytesEncryptor;
 import org.springframework.security.crypto.encrypt.BytesEncryptor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class AuthConfig {
@@ -15,5 +17,10 @@ public class AuthConfig {
   @Bean
   public BytesEncryptor bytesEncryptor() {
     return new AesBytesEncryptor(secret, "12362819");
+  }
+
+  @Bean
+  PasswordEncoder passwordEncoder() {
+    return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
   }
 }
