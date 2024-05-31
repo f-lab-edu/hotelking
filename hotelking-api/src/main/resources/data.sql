042fc544755282ceedcede0ea3967dd54bba8e68
김민식 <kmss69052@naver.com>
2024-05-31 20:28:10 +0900

    Feat: [hotelking-api] 약관동의 data.sql 추가

---
+++ hotelking-api/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+-- 약관동의
+INSERT INTO AGREEMENT (id, agreement_name, agreement_content, agreement_mandatory, created_at, updated_at) VALUES (1, 'TERMS_OF_SERVICE', '이용 약관 동의 내용', 1, NOW(), NOW());
+INSERT INTO AGREEMENT (id, agreement_name, agreement_content, agreement_mandatory, created_at, updated_at) VALUES (2, 'PRIVACY_POLICY', '개인정보 처리 방침 동의', 1, NOW(), NOW());
+INSERT INTO AGREEMENT (id, agreement_name, agreement_content, agreement_mandatory, created_at, updated_at) VALUES (3, 'MARKETING', '마케팅 정보 수신 동의', 0, NOW(), NOW());
+INSERT INTO AGREEMENT (id, agreement_name, agreement_content, agreement_mandatory, created_at, updated_at) VALUES (4, 'THIRD_PARTY_SHARING', '제3자 정보 제공 동의', 0, NOW(), NOW());
+INSERT INTO AGREEMENT (id, agreement_name, agreement_content, agreement_mandatory, created_at, updated_at) VALUES (5, 'LOCATION_SERVICES', '위치 정보 이용 동의', 0, NOW(), NOW());
+INSERT INTO AGREEMENT (id, agreement_name, agreement_content, agreement_mandatory, created_at, updated_at) VALUES (6, 'NEWSLETTER', '뉴스레터 수신 동의', 0, NOW(), NOW());
+
+
