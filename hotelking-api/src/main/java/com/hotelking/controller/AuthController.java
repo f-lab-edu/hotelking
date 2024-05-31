ab1d1b560cb3286a9250d79cb4b26f98dd496be0
kmss6905 <kmss69052@naver.com>
2024-05-31 14:41:47 +0900

    Feat: [hotelking-api] 휴대폰 인증번호 컨트롤러 추가

--- hotelking-api/src/main/java/com/hotelking/controller/AuthController.java
+++ hotelking-api/src/main/java/com/hotelking/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.hotelking.controller;
 import com.hotelking.application.AuthService;
 import com.hotelking.dto.auth.PhoneAuthToken;
 import com.hotelking.dto.request.PhoneAuthCodeRequest;
+import com.hotelking.dto.request.PhoneAuthConfirmRequest;
 import com.hotelking.response.ApiResponse;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,10 +18,17 @@ public class AuthController {
     this.authService = authService;
   }
 
-  @PostMapping(value = "/phone-verification")
+  @PostMapping( "/phone-verification")
   public ApiResponse<PhoneAuthToken> verifyPhone(@RequestBody PhoneAuthCodeRequest phoneAuthCodeRequest) {
     phoneAuthCodeRequest.validationCheck();
     PhoneAuthToken phoneAuthToken = authService.issuePhoneAuth(phoneAuthCodeRequest.toDto());
     return ApiResponse.success(phoneAuthToken);
+  }
+
+  @PostMapping("/phone-confirm")
+  public ApiResponse<Void> confirmPhone(@RequestBody PhoneAuthConfirmRequest phoneAuthConfirmRequest) {
+    phoneAuthConfirmRequest.validationCheck();
+    authService.confirmPhoneAuthCode(phoneAuthConfirmRequest.toDto());
+    return ApiResponse.success();
   }
 }
