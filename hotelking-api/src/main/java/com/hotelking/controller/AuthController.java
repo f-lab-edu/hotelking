b53d714f3f76a071c2ffd76d9c1a49f9bfbc050d
김민식 <kmss69052@naver.com>
2024-06-05 01:27:49 +0900

    Feat: [api] phone 인증 시 service type 에 따라 유저 확인 여부 검사하도록 수정

--- hotelking-api/src/main/java/com/hotelking/controller/AuthController.java
+++ hotelking-api/src/main/java/com/hotelking/controller/AuthController.java
@@ -1,6 +1,7 @@
 package com.hotelking.controller;
 
 import com.hotelking.application.AuthService;
+import com.hotelking.application.UserService;
 import com.hotelking.dto.auth.PhoneAuthToken;
 import com.hotelking.dto.request.PhoneAuthCodeRequest;
 import com.hotelking.dto.request.PhoneAuthConfirmRequest;
@@ -13,14 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   private final AuthService authService;
+  private final UserService userService;
 
-  public AuthController(AuthService authService) {
+  public AuthController(AuthService authService, UserService userService) {
     this.authService = authService;
+    this.userService = userService;
   }
 
   @PostMapping( "/phone-verification")
   public ApiResponse<PhoneAuthToken> verifyPhone(@RequestBody PhoneAuthCodeRequest phoneAuthCodeRequest) {
     phoneAuthCodeRequest.validationCheck();
+    if (phoneAuthCodeRequest.authServiceType().isUserCheckServiceType()) {
+      userService.checkExistUserByPhoneNumber(phoneAuthCodeRequest.phoneNumber());
+    }
     PhoneAuthToken phoneAuthToken = authService.issuePhoneAuth(phoneAuthCodeRequest.toDto());
     return ApiResponse.success(phoneAuthToken);
   }
