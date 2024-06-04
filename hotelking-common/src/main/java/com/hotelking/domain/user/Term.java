5bc302964b24458813c87049eb451ac7edb0b0d9
김민식 <kmss69052@naver.com>
2024-06-04 13:47:58 +0900

    Feat: [hotelking-common] Agreement -> Term 엔티티 이름 변경

--- hotelking-common/src/main/java/com/hotelking/domain/user/Term.java
+++ hotelking-common/src/main/java/com/hotelking/domain/user/Term.java
@@ -1,33 +1,20 @@
 package com.hotelking.domain.user;
 
 import com.hotelking.domain.BaseTimeEntity;
-import com.hotelking.domain.user.vo.AgreementName;
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import java.util.ArrayList;
-import java.util.List;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-// Term
+@Getter
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class Agreement extends BaseTimeEntity {
+public class Term extends BaseTimeEntity {
 
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-
-  @Enumerated(value = EnumType.STRING)
-  @Column(name = "agreement_name", nullable = false, unique = true)
-  private AgreementName name;
+  @EmbeddedId
+  private TermId termId;
 
   @Column(name = "agreement_content", nullable = false)
   private String content;
@@ -35,8 +22,9 @@ public class Agreement extends BaseTimeEntity {
   @Column(name = "agreement_mandatory", nullable = false)
   private boolean isMandatory;
 
-  @OneToMany(cascade = CascadeType.REMOVE, orphanRemoval = true, mappedBy = "agreement")
-  private List<AgreementVersion> versions = new ArrayList<>();
-
-  // agreement name , agreement version -> 복합키 로 식별가능, 이러면 굳이 id 가 필요없다., 오버엔지니어링
+  public Term(TermId termId, String content, boolean isMandatory) {
+    this.termId = termId;
+    this.content = content;
+    this.isMandatory = isMandatory;
+  }
 }
