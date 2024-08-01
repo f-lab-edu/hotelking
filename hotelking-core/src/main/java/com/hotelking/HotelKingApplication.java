9f91f89ceea8c521fad7a2561d3b9a10dd1f5629
김민식 <kmss69052@naver.com>
2024-08-02 01:31:31 +0900

    chore: core, redis yml 추가

---
+++ hotelking-core/src/main/java/com/hotelking/HotelKingApplication.java
@@ -0,0 +1,13 @@
+package com.hotelking;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelKingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HotelKingApplication.class, args);
+  }
+}
